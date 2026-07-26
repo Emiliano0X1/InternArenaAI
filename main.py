eba1e5435bd0667ba28e33911a16aa24a6ca30c8
Emiliano0X1 <egp2506@gmail.com>
2026-07-25 20:34:12 -0700

    updated routes for better backend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers.player import router
+from routers.matchPred import router
 import uvicorn
 
 app = FastAPI()
@@ -8,7 +8,7 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello World'}
 
-app.include_router(router, prefix="/players")
+app.include_router(router, prefix="/match")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- routers/matchPred.py
+++ routers/matchPred.py
@@ -1,13 +1,23 @@
 from fastapi import APIRouter  # type: ignore[reportMissingImports]
+from typing import Any
 
 from numpy import *
 from typing import List
 from models.playerItem import Player
 from services.heuristic_algo_service import predict_winner_heuristic
 
-
 router = APIRouter(prefix="/players", tags=["players"])
 
 @router.post("/predict")
 async def getPredictionResults(players : List[Player]): 
     return predict_winner_heuristic(players)
+
+#TODO -> Endpoint to get the players data from the LeetcodeAPI and return it to frontend
+@router.get("/stats")
+async def getStatsFromPlayers(usernames : List[str]):
+    pass
+
+#TODO -> Endpoint to store the ranking and is_new when the party is ended
+@router.post("/history")
+async def storedHistoryData(matchHistory : Any):
+    pass
